8cfdf3105178c00a3c08dd47d730a837a724ddee
Jefferson Dos Anjos Santos <20211101110051@ifrn.local>
2025-05-07 14:22:57 -0300

    Primeiro commit

---
+++ Aula1/src/MysFirstComponent.jsx
@@ -0,0 +1,16 @@
+function MyFirstComponent(){
+    const nome = "Jefferson"
+        
+    return (
+        <>
+        
+        <div>
+            <h1>olá mundo, seja bem-vindo a interface</h1>
+        </div>
+        <div>
+           <p>Seu nome é    {nome}</p> 
+        </div>
+        </>
+    );
+}
+export default MyFirstComponent
