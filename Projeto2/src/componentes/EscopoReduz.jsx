ba93850ab0b3d120492b5642f414b698c597bab7
Jefferson Dos Anjos Santos <20211101110051@ifrn.local>
2025-05-08 17:48:46 -0300

    Atualização

--- Projeto2/src/componentes/EscopoReduz.jsx
+++ Projeto2/src/componentes/EscopoReduz.jsx
@@ -1,8 +1,20 @@
 const EscopoReduz = () => {
+    function handleClick (){
+        return (
+            alert ("clicaste de novo boy")
+        )
+    }
     return(
         <>
         <button onClick={() => console.log("Você clicou")}>Clique aqui</button>
-        
+        <div>
+            <button onClick={() => alert("Você clicou")}>Novo botão</button>
+        </div>
+        <div>
+            <button onClick={handleClick}>Botão 3</button>
+        </div>
+
+        <div className="container" onClick={() => alert("container")}></div>
         </>
     )
 }
