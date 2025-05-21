4ee5de5d8281e35a387d0e17781fcec6e1c5aa95
Jeffin18 <jefferson.anjos@escolar.ifrn.edu.br>
2025-05-21 14:08:25 -0300

    atualizei

--- Projeto4/src/App.jsx
+++ Projeto4/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Props from './componentes/Props'
+import foto from './assets/recruta.jpg'
 
 function App (){
   let aluno = 'Dos Anjos'
   return(
     <>
-    <Props nome={aluno} idade={18} />
+    <Props nome={aluno} idade={19} numero={24} foto={foto} descricao="Foto minha"/>
+
     </>
   )
 }
--- Projeto4/src/componentes/Props.jsx
+++ Projeto4/src/componentes/Props.jsx
@@ -1,13 +1,15 @@
 //export default function Props(props){
 
-export default function Props({nome, idade}){ //exemplo de desestruturação de props, trabalha-se em forma de variáveis
+export default function Props({nome, idade, numero, foto, descricao}){ //exemplo de desestruturação de props, trabalha-se em forma de variáveis
     return(
         <>
             <div>
                 <h2>Teste Props</h2>
-                <h2>Ola {nome}, {idade}</h2>
-                <img src="foto" alt={nome} />
-
+                <h2>Olá {nome} sua idade é {idade}, seu número de guerra é {numero}</h2>
+                <figure>
+                    <img src={foto} alt={nome}/>
+                    <figcaption>{descricao}</figcaption>
+                </figure>
             </div>
         </>
 
